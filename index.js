95e5788fbec4e3d86c1721625dbce52cba971764
WebersIO <muzammilmetar82@gmail.com>
2025-02-10 17:40:40 +0530

    mujju

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
-let count = "russia";
+// let count = "russia";
 
-const api_url = `https://covid-193.p.rapidapi.com/statistics?country=all`;
-const country_url = `https://covid-193.p.rapidapi.com/statistics`;
-const options = {
-  method: "GET",
-  headers: {
-    "x-rapidapi-key": "d62844f455msh2195c074f0292d4p139cd5jsn1d0960856900",
-    "x-rapidapi-host": "covid-193.p.rapidapi.com",
-  },
-};
+// let opt = document.getElementById("option");
 
-fetch(api_url, options)
+const api_url = `https://api.rootnet.in/covid19-in/stats/latest`;
+// const country_url = `https://covid-193.p.rapidapi.com/statistics`;
+
+
+fetch(api_url)
   .then((res) => res.json())
   .then((data) => {
-    // console.log(data.response[0]);
-    showdata(data.response[0]);
-  });
-
-document.getElementById("dates").addEventListener("change", (e) => {
-  let date = e.target.value;
-  console.log(date);
-});
-
-
-fetch(country_url, options)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data.response[0]);
-    showcountry(data.response[0]);
-  });
+    console.log(data);
+    showdata(data.data.regional);
+  })
 
 document.getElementById("dates").addEventListener("change", (e) => {
   let date = e.target.value;
@@ -36,20 +19,18 @@ document.getElementById("dates").addEventListener("change", (e) => {
 });
 
 function showdata(data) {
-  document.getElementById("display").innerHTML += `
-<h1>World Wild Real-Time Data of <br/>${data.day}</h1>
-<div><b>active:  </b>${data.cases.active}</div>
-<div><b>new: </b> ${data.cases.new}</div>
-<div><b>critical: </b> ${data.cases.critical}</div>
-<div><b>recovered: </b> ${data.cases.recovered}</div>
-<div><b>total: </b> ${data.cases.total}</div>
-<div><b>deaths: </b> ${data.deaths.total}</div>
+
+  data.map((ele) => {
+    console.log(ele)
+      document.getElementById("display").innerHTML += `
+<h3>covid data of <br/>  "${ele.loc}"</h3>
+<div><b>critical: </b> ${ele.totalConfirmed}</div>
+<div><b>indian cases:  </b>${ele.confirmedCasesIndian}</div>
+<div><b>discharges: </b> ${ele.discharged}</div>
+<div class="mb-4"><b>deaths: </b> ${ele.deaths}</div>
 `;
+  })
+
 }
 
-function showcountry(data) {
-  document.getElementById("option").innerHTML += `
-                <option value=${data}>${data}</option>
 
-`;
-}
