950ce7655f7a550393e9c83e34d91014386b8132
WebersIO <muzammilmetar82@gmail.com>
2025-02-10 18:30:41 +0530

    completed

--- index.js
+++ index.js
@@ -2,26 +2,41 @@
 
 // let opt = document.getElementById("option");
 
+// let search = document.getElementById("dates").value
+
 const api_url = `https://api.rootnet.in/covid19-in/stats/latest`;
 // const country_url = `https://covid-193.p.rapidapi.com/statistics`;
-
 
 fetch(api_url)
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
-    showdata(data.data.regional);
-  })
 
-document.getElementById("dates").addEventListener("change", (e) => {
-  let date = e.target.value;
-  console.log(date);
-});
+    let search = "Delhi";
 
-function showdata(data) {
+    document.getElementById("dates").addEventListener("change", (e) => {
+      search = e.target.value;
+     
 
-  data.map((ele) => {
-    console.log(ele)
+      let s_search = search.split(" ")
+
+      search = s_search.map((ele) => {
+      let val = ele.charAt(0).toUpperCase() + ele.slice(1);
+      return val
+     }).join(" ")
+
+      console.log(search);
+       showdata(data.data.regional, search)
+    });
+  });
+
+function showdata(data, s) {
+  document.getElementById("display").innerHTML = " ";
+
+  data
+    .filter((el) => el.loc == s.charAt(0).toUpperCase() + s.slice(1))
+    .map((ele) => {
+      console.log(ele);
       document.getElementById("display").innerHTML += `
 <h3>covid data of <br/>  "${ele.loc}"</h3>
 <div><b>critical: </b> ${ele.totalConfirmed}</div>
@@ -29,8 +44,5 @@ function showdata(data) {
 <div><b>discharges: </b> ${ele.discharged}</div>
 <div class="mb-4"><b>deaths: </b> ${ele.deaths}</div>
 `;
-  })
-
+    });
 }
-
-
